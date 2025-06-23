58aa0edefbae8a6f8f0b156d26898159b545f8a8
Faijur199 <faijurahaman4@gmail.com>
2025-06-23 12:50:46 +0545

    database

---
+++ backend/index.php
@@ -0,0 +1,31 @@
+<?php
+$con = mysqli_connect("localhost","root","","wits");
+$response = array();
+if($con){
+    $sql = "select * from `track` ";
+    $result = mysqli_query($con,$sql);
+    if($result){    
+        header("Content-Type: JSON");
+        $i=0;
+        while($row = mysqli_fetch_assoc($result)){
+           $response[$i]['ID'] = $row['ID'];
+           $response[$i]['Item_Name'] = $row['Item_Name'];
+           $response[$i]['Person_Name'] = $row['Person_Name'];
+           $response[$i]['Location'] = $row['Location'];
+           $response[$i]['Lent'] = $row['Lent'];
+           $response[$i]['Date'] = $row['Date'];
+           $i++;
+
+        }
+        echo json_encode($response,JSON_PRETTY_PRINT);
+    }
+    else{
+        echo "Query Failed!";
+    }
+}
+else{
+    echo "Database Connection Failed";
+}
+?>
+
+
